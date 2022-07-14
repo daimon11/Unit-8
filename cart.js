5f784856c2b0431d8408d1ac778b5f80fb106db7
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-07-14 13:18:17 +0300

    14-07-22(13-18)

--- cart.js
+++ cart.js
@@ -3,22 +3,44 @@ const cart = {
   totalPrice: 0,
   count: 0,
   getTotalPrice() {
+    this.totalPrice += this.calculateItemPrice()
     return this.totalPrice;
   },
-  add: function (productName, priceProduct, quantityProduct) {
-    this.items.push(productName);
-    this.totalPrice += priceProduct*quantityProduct;
-    this.count += quantityProduct;
+  add(productName, priceProduct, quantityProduct) {
+    let product = {};
+    priceProduct = +priceProduct;
+    if (quantityProduct === undefined || quantityProduct === '' || quantityProduct === null || Number.isNaN(quantityProduct)) {
+      product = {
+        productName: productName,
+        priceProduct: +priceProduct,
+        quantityProduct: 1,
+      };
+      this.count += 1;
+    } else {
+      product = {
+        productName: productName,
+        priceProduct: +priceProduct,
+        quantityProduct: +quantityProduct,
+      };
+      this.count += quantityProduct;
+    }
+    this.items.push(product);
   },
   increaseCount(number) {
-    for (i = 0; i < number; i++) {
-      this.count *= number[i]
-    };
+    if (number === undefined || number === '' || number === null || Number.isNaN(number)) {
+      this.count += 0;
+    } else {
+      this.count += number;
+      this.items[cart.items.length - 1].quantityProduct += number;
+    }
     return this.count;
   },
   calculateItemPrice() {
-    this.totalPrice += priceProduct * quantityProduct;
-    return this.totalPrice;
+    let rest = [];
+    for (let i = 0; i < this.items.length; i++) {
+      rest[i] = [this.items[i].priceProduct * this.items[i].quantityProduct];
+    }
+    return (rest.flat().reduce((sum, current) => sum + current, 0));
   },
   clear() {
     this.items = [];
@@ -26,19 +48,18 @@ const cart = {
     this.count = 0;
   },
   print() {
-console.log(JSON.stringify(this.items));
-console.log(JSON.stringify(this.totalPrice));
+    console.log(JSON.stringify(this.items));
+    console.log(JSON.stringify(this.totalPrice));
   },
 }
 
-console.log(cart);
-console.log(cart.totalPrice);
-cart.add('Пиво', 50, 2);
-cart.add('Сидр', 70, 3);
-cart.print();
-cart.add('Вино', 550, 5);
-cart.add('Квас', 105, 1);
-console.log(cart);
-cart.print();
-cart.clear();
-console.log(cart);
+cart.add('Пиво', 100, 3);
+cart.add('Cидр', 75);
+cart.increaseCount(7);
+cart.add('Квас', 80, 2);
+console.log('Предварительная сумма товаров = ' + cart.calculateItemPrice());
+console.log(cart.items);
+console.log('Общее количество товаров в корзине = ' + cart.count);
+console.log('Количество товаров последнего добавленного товара = ' + cart.items[cart.items.length - 1].quantityProduct);
+console.log('Общая сумма товаров = ' + cart.getTotalPrice());
+
